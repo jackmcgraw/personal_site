d64a9a3021c2f24ad4d649175a01dbf01d05261e
John McGraw <john.s.mcgraw.20@dartmouth.edu>
2018-06-08 11:34:06 -0700

    page counter in seconds

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('starting up!');
+
+// change require to es6 import style
+import $ from 'jquery';
+
+$('#main').html('Here we go!');
+$('#seconds_counter').html('Current seconds...');
+
+// from https://stackoverflow.com/questions/37187504/javascript-second-counter
+var seconds = 0;
+function incrementSeconds() {
+    seconds += 1;
+    $('#seconds_counter').html("You have been here for " + seconds + " seconds.");
+}
+
+var cancel = setInterval(incrementSeconds, 1000);
